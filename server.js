76a23b85eab4bc692619373afc7a85bcea09010f
Slammad <saidubello212@gmail.com>
2019-06-06 14:54:34 +0100

    Installing neccessary node modules

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const port = process.env.PORT || 3000;
+
+const server = http.createServer()
+
+server.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
