324ce41a5f175547ac5173994452c8ac8d11a944
Slammad <saidubello212@gmail.com>
2019-06-06 16:29:29 +0100

    Initialized a node server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const port = process.env.PORT || 3000;
-
-const server = http.createServer()
+const app = require('./app')
+const server = http.createServer(app)
 
 server.listen(port,()=>{
     console.log(`Listening on port ${port}`)
